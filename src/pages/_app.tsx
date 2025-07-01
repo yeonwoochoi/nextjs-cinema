1d26cef18363cbdd0f0851197b1450d641187840
beagle <chldusdn20@gmail.com>
2025-07-01 10:58:54 +0900

    chore: replace empty object type with Record<string, unknown> in NextPageWithLayout
    - Ensures stricter and safer typing for page props
    - Prevents unintended values like 0 or "" from passing type checks

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import GlobalLayout from "@/components/global-layout";
 import { NextPage } from "next";
 import { ReactElement, ReactNode } from "react";
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
 
