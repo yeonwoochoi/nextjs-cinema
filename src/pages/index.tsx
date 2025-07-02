e12688fd425ef31b0f24ef3ecd66f7ceba6f48b2
beagle <chldusdn20@gmail.com>
2025-07-02 11:05:34 +0900

    feat: add loading component in index page during fetching data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { ReactElement } from "react";
+import Head from 'next/head'
+import { GetStaticProps } from "next";
+import { useRouter } from "next/router";
 import SearchableLayout from "@/components/searchable-layout";
 import MovieItem from "@/components/movie-item";
+import ErrorMessage from "@/components/error-message";
+import LoadingMessage from "@/components/loading-message";
 import fetchRandomMovies from "@/lib/fetch-random-movies";
 import fetchMovies from "@/lib/fetch-movies";
 import { MovieData } from "@/types/types";
-import { GetStaticProps } from "next";
-import ErrorMessage from "@/components/error-message";
-import Head from 'next/head'
 
 interface HomePageProps {
   allMovies: MovieData[],
@@ -49,6 +51,11 @@ export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
 export default function Home({ allMovies, recoMovies, error }: HomePageProps) {
   if (error) {
     return <ErrorMessage error={error} />
+  }
+
+  const router = useRouter()
+  if (router.isFallback) {
+    return <LoadingMessage />
   }
 
   return (
