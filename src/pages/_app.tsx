abbd48a932ec66c62744c30cc739f3aa9999089f
beagle <chldusdn20@gmail.com>
2025-07-02 11:25:50 +0900

    fix: move viewport meta tag from _document.js to _app.tsx to avoid warning

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import GlobalLayout from "@/components/global-layout";
 import { NextPage } from "next";
 import { ReactElement, ReactNode } from "react";
+import Head from "next/head";
 
 export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -15,8 +16,13 @@ type AppPropsWithLayout = AppProps & {
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
   return (
-    <GlobalLayout>
-      {getLayout(<Component {...pageProps} />)}
-    </GlobalLayout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <GlobalLayout>
+        {getLayout(<Component {...pageProps} />)}
+      </GlobalLayout>
+    </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <body>
         <Main />
